f5f3c6ff60aab6c3cb4f95b95ada866d7a823143
Leonie Ziechmann <48921293+leonieziechmann@users.noreply.github.com>
2026-05-04 12:51:01 +0200

    Added Flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Genesis Oracle Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          uv
+          git
+          python311
+        ];
+
+        shellHook = ''
+          export UV_PYTHON_DOWNLOADS=never
+          export UV_PYTHON=python3
+        '';
+      };
+    };
+}
